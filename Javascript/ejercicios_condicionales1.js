c866234e79e09a4fa07b6b019c335a09475b3d8a
Sofiaraigosa <hrsofia17@gmail.com>
2024-09-05 18:31:25 -0500

    react activity done

--- Javascript/ejercicios_condicionales1.js
+++
@@ -1,8 +0,0 @@
-let puntaje = 75;
-if(puntaje >= 90){
-    console.log("Excelente");
-}else if (puntaje >= 70 && puntaje <= 89 ){
-    console.log ("Bueno");
-}else{
-    console.log("Necesita mejorar");
-}
--- Javascript/ejercicios_condicionales2.js
+++
@@ -1,8 +0,0 @@
-let hora =10;
-if (hora >= 6 && hora <12){
-    console.log("Buenos dias");
-}else if (hora >=12 && hora <=18){
-    console.log("Buenas tardes");
-}else {
-    console.log("Buenas noches");
-}
